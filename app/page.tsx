ea4002df3019471b4df177298388659112e2d8d0
cozyCr4te <devsepnine@hotmail.com>
2024-05-28 16:35:15 +0900

    fix: Remove RefreshProvider.tsx
    - ADD cache-control option(staleTimes)
    - Remove RefreshProvider

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { RefreshProvider } from '@/layout/RefreshProvider/RefreshProvider.tsx';
 import { Home } from '@/page/home/Home.tsx';
 
 export default async function RootPage() {
-  return (
-    <RefreshProvider>
-      <Home />
-    </RefreshProvider>
-  );
+  return <Home />;
 }
